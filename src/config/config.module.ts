eb007d47d1867060edabdf377638c763519e6047
Hazi <83254065@qq.com>
2024-06-21 15:41:31 +0800

    docs: update md

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -2,10 +2,6 @@ import { Module } from "@nestjs/common";
 
 @Module({
     imports: [
-        ConfigModule.forRoot({
-            envFilePath: ['.env.development.local', '.env.development'],
-            isGlobal: true,
-        })
     ],
 })
 export class ConfigModule {}
